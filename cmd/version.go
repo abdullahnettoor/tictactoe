ee7979324415a65217d43151de03883cb1094c40
abdullahnettoor <abdullahnettoor@gmail.com>
2024-10-29 13:39:04 +0530

    feat: add version command and initial version.
    - Add a `version` command to print the current version of the application.
    - Set the initial version to `v0.1.0`.
    
    Signed-off-by: abdullahnettoor <abdullahnettoor@gmail.com>

---
+++ cmd/version.go
@@ -0,0 +1,29 @@
+/*
+Copyright © 2024 Abdullah Nettoor abdullahnettoor@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// Version holds the current version of the application
+	Version = "v0.1.0"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of TicTacToe",
+	Long:  `All software has versions. This is TicTacToe's.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("TicTacToe %s\n", Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
